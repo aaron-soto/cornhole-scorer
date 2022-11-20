bb8c02ce0d120d28eddb7d9bf4f30f56837a0111
Aaron Soto <aaron.m.soto1@gmail.com>
2022-11-20 12:24:11 -0700

    init

---
+++ index.js
@@ -0,0 +1,117 @@
+let scoreBtns = document.querySelectorAll('.score-btn');
+let nextRoundBtn = document.getElementById('next-round');
+let resetBtn = document.getElementById('reset');
+let redScoreText = document.querySelector('.score.red');
+let blueScoreText = document.querySelector('.score.blue');
+
+let scores = {
+	red: 0,
+	blue: 0,
+};
+let roundScores = {
+	red: {
+		on: 0,
+		in: 0,
+	},
+	blue: {
+		on: 0,
+		in: 0,
+	},
+};
+
+const handleScoreTouch = (e) => {
+	e.preventDefault();
+
+	let clickedValue = e.target.innerText;
+	let clickedBtn = e.target;
+	let team = clickedBtn.classList[1];
+	let onOrIn = clickedBtn.classList[2];
+
+	// If button is not active, active it
+	if (!clickedBtn.classList.contains('active')) {
+		// Set other button to in active if there is one
+		if (e.target.parentElement.querySelectorAll('.active')[0]) {
+			e.target.parentElement
+				.querySelectorAll('.active')[0]
+				.classList.remove('active');
+		}
+
+		roundScores[`${team}`][`${onOrIn}`] = parseInt(clickedValue);
+
+		clickedBtn.classList.add('active');
+		console.log(roundScores);
+	}
+};
+
+const handleNextRound = (e) => {
+	e.preventDefault();
+
+	// Set total Scores
+	scores = {
+		red: (scores.red += roundScores.red.on + roundScores.red.in * 3),
+		blue: (scores.blue += roundScores.blue.on + roundScores.blue.in * 3),
+	};
+
+	console.log(scores);
+	// Reset Round Score Values
+	roundScores = {
+		red: {
+			on: 0,
+			in: 0,
+		},
+		blue: {
+			on: 0,
+			in: 0,
+		},
+	};
+
+	// Set total scores text values
+	setScoreTextsToValue();
+
+	// Reset buttons
+	resetAllButtons();
+};
+
+const handleReset = (e) => {
+	e.preventDefault();
+
+	resetAllButtons();
+
+	scores = {
+		red: 0,
+		blue: 0,
+	};
+	roundScores = {
+		red: {
+			on: 0,
+			in: 0,
+		},
+		blue: {
+			on: 0,
+			in: 0,
+		},
+	};
+
+	setScoreTextsToValue();
+};
+
+const resetAllButtons = () => {
+	let activeBtns = document.querySelectorAll('.score-btn.active');
+	activeBtns.forEach((btn) => btn.classList.remove('active'));
+};
+
+const setScoreTextsToValue = () => {
+	redScoreText.innerText = scores.red.toString();
+	blueScoreText.innerText = scores.blue.toString();
+};
+
+resetBtn.addEventListener('touchstart', handleReset);
+resetBtn.addEventListener('click', handleReset);
+
+nextRoundBtn.addEventListener('touchstart', handleNextRound);
+nextRoundBtn.addEventListener('click', handleNextRound);
+
+scoreBtns.forEach((btn) => {
+	btn.addEventListener('touchstart', handleScoreTouch);
+	btn.addEventListener('click', handleScoreTouch);
+});
